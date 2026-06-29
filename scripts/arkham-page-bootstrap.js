a11136b7169d198edf6d3d9f4926fd009f848648
BGB <2018zhangyan@gmail.com>
2026-06-29 06:06:31 -0400

    .

--- scripts/arkham-page-bootstrap.js
+++ scripts/arkham-page-bootstrap.js
@@ -41,7 +41,7 @@
 
   const stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
-  stylesheet.href = "/styles/pages/arkham_horror_lcg.css?v=20260628a";
+  stylesheet.href = "/styles/pages/arkham_horror_lcg.css?v=20260629a";
   document.head.appendChild(stylesheet);
 
   const root = document.createElement("div");
